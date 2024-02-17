5795636441897c615efbdc56b6bf1b50053d61c4
RabbyHasanSaref <rabbyhasansaref83@gmail.com>
2024-02-17 17:06:08 +0600

    Game complete

--- js/app.js
+++ js/app.js
@@ -12,9 +12,28 @@ function setKyeBgColor(element){
     const alphabetColor = document.getElementById(element);
     alphabetColor.classList.add('bg-orange-300');
 }
+
 function removeKyeBgColo(element){
     const alphabetColor = document.getElementById(element);
     alphabetColor.classList.remove('bg-orange-300');
+}
+
+function getLifeScoreValueId(element){
+    const elementById = document.getElementById(element);
+    const elementValueTaxt = elementById.innerText;
+    const elementValue = parseInt(elementValueTaxt);
+    return elementValue;
+}
+
+function getLifeScoreValue(element, value){
+    const elementById = document.getElementById(element);
+    elementById.innerText = value;
+}
+
+function getLifeScore(element){
+    const elementById = document.getElementById(element);
+    const elementText = elementById.innerText;
+    return elementText;
 }
 
 function alphabetRandom(){
--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 function Play(){
-    // const firstPart = document.getElementById('first-part');
-    // firstPart.classList.add('hidden');
     hiddenSection('first-part');
+    hiddenSection('thred-part')
     showSection('second-part');
-
-    // const secondPart = document.getElementById('second-part');
-    // secondPart.classList.remove('hidden')
+    getLifeScoreValue('life', 5);
+    getLifeScoreValue('score', 0)
     continuesGame()
 }
 
@@ -14,4 +12,43 @@ function continuesGame(){
     const artShow = document.getElementById('art-show');
     artShow.innerText = alphabet;
     setKyeBgColor(alphabet);
+}
+
+document.addEventListener('keyup', handlerEventButton)
+function handlerEventButton(event){
+    const gamerPrass = event.key;
+   const gamerPressElement = document.getElementById('art-show');
+   const gamerePrassElementData = gamerPressElement.innerText;
+   const gamerText = gamerePrassElementData.toLowerCase();
+   if(gamerPrass === 'Escape'){
+    gameOver();
+    }
+   if(gamerPrass === gamerText){
+    const currentScore = getLifeScoreValueId('score');
+    const updatedScore = currentScore + 1;
+    getLifeScoreValue('score', updatedScore);
+
+    removeKyeBgColo(gamerText);
+    continuesGame()
+   }
+   else{
+    const currentLife = getLifeScoreValueId('life');
+    const updatedLife = currentLife - 1;
+    getLifeScoreValue('life', updatedLife);
+
+    if(updatedLife === 0){
+        gameOver();
+    }
+   }
+}
+
+function gameOver(){
+    hiddenSection('second-part')
+    showSection('thred-part');
+
+    const gameScore = getLifeScoreValueId('score');
+    getLifeScoreValue('game-score', gameScore);
+    
+    const gameScores =  getLifeScore('art-show');
+    removeKyeBgColo(gameScores);
 }
